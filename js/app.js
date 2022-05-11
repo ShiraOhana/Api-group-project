07f65f0ea9b40fad8867285acbe96841b8935d26
EyalSinay <eyalsinay@gmail.com>
2022-05-11 13:23:58 +0300

    get data

---
+++ js/app.js
@@ -0,0 +1,3 @@
+import { getAllStudents } from "./load_all_data.js";
+
+getAllStudents().then(console.log);
---
+++ js/load_all_data.js
@@ -0,0 +1,42 @@
+async function addAllPropertyToStudents(arr) {
+    arr.forEach(async obj => {
+        const response = await fetch(`https://capsules-asb6.herokuapp.com/api/user/${obj.id}`);
+        const studentProperty = await response.json();
+        obj.gender = studentProperty.gender;
+        obj.hobby = studentProperty.hobby;
+        obj.age = studentProperty.age;
+        obj.city = studentProperty.city;
+        obj.capsule = studentProperty.capsule;
+    });
+}
+
+async function getMordyStudents() {
+    try {
+        const response = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/mordi");
+        const mordyStudents = await response.json();
+        await addAllPropertyToStudents(mordyStudents);
+        return mordyStudents;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function getToamStudents() {
+    try {
+        const response = await fetch("https://capsules-asb6.herokuapp.com/api/teacher/toam");
+        const toamStudents = await response.json();
+        await addAllPropertyToStudents(toamStudents);
+        return toamStudents;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getAllStudents() {
+    const mordyStudentsPromise = getMordyStudents();
+    const toamStudentsPromise = getToamStudents();
+    const mordyStudents = await mordyStudentsPromise;
+    const toamStudents = await toamStudentsPromise;
+    const allStudents = mordyStudents.concat(toamStudents);
+    return allStudents;
+}
